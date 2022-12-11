429e5fb6a0c11bdd5806800d97e16a6fd3ee6540
Artem Dyrdin <74466573+ArtemDyrdin@users.noreply.github.com>
2022-12-11 15:18:51 +0300

    Добавьте файлы проекта.

---
+++ practice2/practice2.cpp
@@ -0,0 +1,167 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+void fill_arrays(string number, int* number_arr) {
+    // если длина строки не более 1000 (по условию задачи)
+    if (number.length() < 1001) {
+        int k = 0;
+        for (int i = number.length() - 1; i >= 0; i--) {
+            number_arr[k] = number[i] - 0x30; // записываем символы строки в массив как число с конца
+            k += 1;
+        }
+    }
+}
+
+void fix_result(string &result) {
+    cout << result << endl;
+    while (result[0] == '0'){
+        result.erase(0, 1);
+        if (result.length() == 1) {
+            result = "0";
+            break;
+        }
+    }
+}
+
+void addition(int* A, int* B, int lengthA, int lengthB) {
+    // операция сложения
+
+    int C[1001] = { 0 }; // создаем массив суммы (на 1 элемент больше для макс больших A и B)
+
+    int length = 0; // длина 
+
+    // ищем массив с макс длиной
+    if (lengthA > lengthB) {
+        length = lengthA;
+    }
+    else {
+        length = lengthB;
+    }
+
+    int c = 0; // индекс C
+    bool k = 0; // добавка к след. разряду
+
+    // повторяем на один раз больше, чем длина макс массива, т.к. сумма может быть на 1 разряд больше
+    for (int i = 0; i < length + 1; i++) {
+
+        int summ = 0; // значение разряда
+
+        // недостающий (пустой) элемент массива приравниваем к 0 (пустой элемент имеет значение меньше 0)
+        if (A[i] < 0) {
+            A[i] = 0;
+        }
+        if (B[i] < 0) {
+            B[i] = 0;
+        }
+
+        cout << A[i] << "\t" << B[i] << endl;
+
+        summ = A[i] + B[i] + k;
+
+        // значение разряда равно остатку суммы двух слагаемых и добавки от прошлой операции
+        C[c] = summ % 10;
+
+        k = summ / 10;
+
+        c += 1;
+    }
+
+    string result;
+
+    for (int i = c-1; i >= 0; i--) {
+        result.push_back(C[i] + '0');
+    }
+
+    fix_result(result);
+
+    cout << "\t= " << result << endl;
+}
+
+void multiplication(int* A, int* B, int lengthA, int lengthB) {
+    // операция умножения
+
+    int C[2000] = { 0 }; // создаем массив произведения (длина массива может достигать 2000 при макс A и B)
+
+    int k = 0; // добавка к след. разряду
+    int c = 0; // индекс массива C
+
+    int digit = 0; // сдвиг на разряд
+
+    for (int i = 0; i < lengthA + lengthB; i++) {
+        for (int j = 0; j < lengthB; j++) {
+            int mlt = 0; // значение разряда
+
+            int add_to_digit = 0; // добавка к следующему разряду из предыдущего разряда
+
+            // придаем недостающему элементу значение 0
+            if (A[i] < 0) {
+                A[i] = 0;
+            }
+            if (B[j] < 0) {
+                B[j] = 0;
+            }
+
+            mlt = (A[i] * B[j]) + k; // произведение элементов массива + добавка\
+                                                                    от значения предыдущей суммы
+
+            C[c + digit] += mlt % 10; // прибавляем к существующему значению разряда значения\
+                                                                    от произведения других элеменотов
+
+            add_to_digit = C[c + digit] / 10; // находим добавку от суммы значений разряда
+
+            C[c + digit] = C[c + digit] % 10; // определяем значение разряда
+
+            C[c + digit + 1] += add_to_digit; // добавляем добавку к след. разряду
+
+            k = mlt / 10;
+
+            c += 1;
+        }
+        digit += 1; // начинаем след. шаг с разряда большего на 1
+        c = 0;
+    }
+
+    string result;
+
+    for (int i = digit; i >= 0; i--) {
+        result.push_back(C[i] + '0');
+    }
+
+    fix_result(result);
+
+    cout << "\t= " << result << endl;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    string A, B; // создаем строки для вводимых чисел
+
+    cout << "Введите числа A и B не более, чем из 1000 цифр" << endl;
+    cout << "A: ";
+    cin >> A;
+    cout << "B: ";
+    cin >> B;
+
+    int A1[1000] = { 0 }; // создаем массивы int для посимвольного хранения чисел
+    int B1[1000] = { 0 };
+
+    fill_arrays(A, A1); // заполняем массивы символами из строки
+    fill_arrays(B, B1);
+
+    int lengthA = A.length(); // определяем длины массивов
+    int lengthB = B.length();
+    
+    cout << A << "\t" << B << endl;
+
+    cout << "Операция сложения:" << endl;
+    addition(A1, B1, lengthA, lengthB);
+
+    cout << endl << "Операция умножения" << endl;
+    multiplication(A1, B1, lengthA, lengthB);
+
+}
---
+++ practice3/practice3.cpp
@@ -0,0 +1,145 @@
+﻿#include "iostream"
+#include <cctype>
+#include <string>
+#include <fstream>
+#include <windows.h>
+
+
+using namespace std;
+
+void count_letters(int letters[32]) {
+	ifstream in("input.txt", ios::in);
+	while (!in.eof()) {
+		unsigned char letter;
+		in >> letter;
+		int code_letter = tolower(letter);
+		letters[code_letter - 224] += 1;
+	}
+}
+
+void find_popular_letters(int letters_counts[32], char letters[32]) {
+	int alphabet = 33;
+	for (int i = 0; i < alphabet + 1; i++)
+	{
+		for (int j = alphabet - 1; j > i; j--)
+		{
+			if (letters_counts[j] > letters_counts[j - 1])
+			{
+				swap(letters_counts[j], letters_counts[j - 1]);
+				
+				swap(letters[j], letters[j - 1]);
+			}
+		}
+	}
+}
+
+bool count_letter(string word, char l) {
+	for (int i = 0; i < word.size(); i++) {
+		if (toupper(word[i]) == l) {
+			return 1;
+		}
+	}
+	return 0;
+}
+
+string tolower_str(string word) {
+	string low_word = "";
+	for (int i = 0; i < word.length(); i++) {
+		low_word.push_back(tolower(word[i]));
+	}
+	return low_word;
+}
+
+string toupper_str(string word) {
+	string up_word = "";
+	for (int i = 0; i < word.length(); i++) {
+		up_word.push_back(toupper(word[i]));
+	}
+	return up_word;
+}
+
+string changed_word(string word, char a, char b) {
+	string new_word = toupper_str(word);
+	char last;
+	bool punct = 0;
+	if (ispunct(new_word[new_word.length() - 1])) { // проверка на знак пунктуации 
+		last = new_word[new_word.length() - 1];
+		new_word.erase(new_word.length()-1, 1);
+		punct = 1;
+	}
+
+	new_word.push_back('(');
+	new_word.push_back(a);
+	new_word.push_back(b);
+	new_word.push_back(')');
+	if (punct) {
+		new_word.push_back(last);
+	}
+
+	return new_word;
+}
+
+string check_word(string word, string popular) {
+	cout << word << endl;
+	for (int a = 0; a < 4; a++) {
+		for (int b = 0; b < 4; b++) {
+			if (popular[a] != popular[b]) {
+				for (int i = 0; i < word.length() - 1; i++) {
+					if (word[i] == popular[a]) {
+						if (word[i + 1] == popular[b]) {
+							return changed_word(word, popular[a], popular[b]);
+						}
+					}
+				}
+			}
+		}
+	}
+	return word;
+}
+
+void compose_output(string popular) {
+	ifstream in("input.txt", ios::in);
+	ofstream out;
+	out.open("output.txt");
+	string line;
+	while (getline(in, line)) {
+		string word;
+		cout << line << endl;
+		for (int i = 0; i < line.length(); i++) {
+			cout << line[i] << endl;
+			word.push_back(line[i]);
+			if (line[i] == ' ' || i == line.length()-1) {
+				out << check_word(tolower_str(word), tolower_str(popular)) << " ";
+				word = "";
+			}
+		}
+		out << "\n";
+	}
+}
+
+int main() {
+
+	setlocale(LC_ALL, "rus");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	char letters[] = { 'а', 'б', 'в', 'г', 'д', 'е', 'ж', 'з', 'и', \
+						'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', \
+							'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', \
+								'э', 'ю', 'я' };
+
+	int letters_counts[32] = { 0 };
+
+	count_letters(letters_counts);
+
+	find_popular_letters(letters_counts, letters);
+
+	string popular = "";
+	for (int i = 0; i < 4; i++) {
+		popular.push_back(letters[i]);
+	}
+
+	compose_output(popular);
+
+    return 0;
+}
