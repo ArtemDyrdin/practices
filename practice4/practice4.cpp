b7918edcf7e6f1d5341733638a94e5a256b20784
Artem Dyrdin <74466573+ArtemDyrdin@users.noreply.github.com>
2022-12-16 22:14:29 +0300

    добавлена практика №4 и №5

---
+++ practice4/practice4.cpp
@@ -0,0 +1,77 @@
+﻿//Целые числа от 0 до 63
+
+#include <iostream>
+#include <bitset>
+#include <vector>
+
+using namespace std;
+
+void compose(vector<char>& numbers){
+
+	string number;
+	for (int i = 0; i < 64; i++) {	// переводим все числа в двоичную систему с макс длиной и складываем как строки
+		string binary = bitset<6>(i).to_string();
+		number += binary;
+	}
+
+	cout << "Все числа в двоичной системе: " << number << endl;
+
+	string num = "";
+	cout << endl << "Разбиваем по 8 знака" << endl;
+	int k = 1;
+	for (int i = 0; i < number.length(); i++) { // разбиваем числа по массиву
+		num += number[i];
+		if ((num.length() % 8 == 0) || i == number.length() - 1) {
+			unsigned char decimal = bitset<8>(num).to_ulong();
+			numbers.push_back(decimal);
+			cout << num << "\t" << k << endl;
+			num = "";
+			k++;
+		}
+	}
+}
+
+void unpucking(vector<char>& numbers) {
+	cout << endl << "unpucking" << endl;
+	string number;
+	for (int i = 0; i < 48; i++) {	// переводим все числа в двоичную систему с макс длиной и складываем как строки
+		string binary = bitset<8>(numbers[i]).to_string();
+		number += binary;
+	}
+	cout << endl << number << endl;
+
+	string num = "";
+	int k = 1;
+	for (int i = 0; i < number.length(); i++) { // разбиваем числа по массиву
+		num += number[i];
+		if ((num.length() % 6 == 0) || i == number.length() - 1) {
+			unsigned int decimal = bitset<6>(num).to_ulong();
+			cout << decimal << endl;
+			num = "";
+			k++;
+		}
+	}
+}
+
+int main()
+{
+	setlocale(LC_ALL, "russian");
+	int bbb[64] = {0};
+	cout << "Исходный размер чисел от 0 до 63: " << sizeof(bbb) << endl;
+
+	// 2^6 = 64
+
+	vector<char> numbers;
+
+	compose(numbers); // сжимаем числа
+
+	for (int i = 0; i < 48; i++) {
+		cout << numbers[i] << "\t" << i+1 << endl;
+	}
+
+	cout << "После сжатия: " << numbers.size() << endl;
+
+	unpucking(numbers);
+
+	return 0;
+}
---
+++ practice5/practice5.cpp
@@ -0,0 +1,118 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class DinArray {
+private:
+    int m_length = 0;
+    int* m_list = new int[m_length];
+
+public:
+    DinArray() {}
+    ~DinArray() {
+        delete[] m_list;
+    }
+
+    void push_back(int el) {
+        // добавление элемента в конец
+        int* new_list = new int[m_length + 1];
+
+        for (int i = 0; i < m_length; i++) {
+            new_list[i] = m_list[i];
+        }
+
+        new_list[m_length] = el;
+
+        delete[] m_list;
+
+        m_list = new_list;
+
+        m_length++;
+    }
+
+    void show_elements() {
+        // вывод всех элементов
+        for (int i = 0; i < m_length; i++) {
+            cout << m_list[i] << " ";
+        }
+        cout << endl;
+    }
+
+    int get_length() {
+        // вернуть количество элементов
+        return m_length;
+    }
+
+    int get_element(int index) {
+        // вернуть элемент по индексу
+        return m_list[index];
+    }
+
+    void replace_element(int index, int el) {
+        // изменить элемент по индексу
+        m_list[index] = el;
+    }
+
+    void delete_element(int index) {
+        // удалить элемент
+        for (int i = 0; i < m_length-1; i++) {
+            if (i >= index) {
+                m_list[i] = m_list[i + 1];
+            }
+        }
+        m_list[m_length - 1] = 0;
+        m_length = m_length - 1;
+    }
+
+    void insert_element(int index, int el) {
+        // вставить элемент по индексу
+        int* new_list = new int[m_length + 1];
+
+        for (int i = 0; i < m_length; i++) {
+            if (i < index) {
+                new_list[i] = m_list[i];
+            }
+            else if (i == index) {
+                new_list[i + 1] = m_list[i];
+                new_list[i] = el;
+            }
+            else if (i > index) {
+                new_list[i+1] = m_list[i];
+            }
+        }
+        delete[] m_list;
+
+        m_list = new_list;
+
+        m_length++;
+    }
+};
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    DinArray List;
+
+    cout << "Добавление элементов в конец массива:" << endl;
+    for (int i = 0; i < 5; i++) {
+        List.push_back(i);
+        List.show_elements();
+    }
+
+    cout << endl << "Кол-во элементов массива: " << List.get_length() << endl;
+
+    cout << endl << "Элемент по индексу: " << List.get_element(2) << endl;
+
+    List.replace_element(0, 10);
+    cout << endl << "Замена элемента:" << endl;
+    List.show_elements();
+
+    cout << endl << "Удаление элемента:" << endl;
+    List.delete_element(2);
+    List.show_elements();
+
+    cout << endl << "Вставка элемента по индексу:" << endl;
+    List.insert_element(2, 22);
+    List.show_elements();
+}
