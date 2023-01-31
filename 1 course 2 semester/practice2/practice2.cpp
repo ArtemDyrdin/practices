e42eda09028879f06f5a6ae527ff5c2940a23346
Artem Dyrdin <74466573+ArtemDyrdin@users.noreply.github.com>
2023-02-01 01:19:33 +0300

    added practice 2

---
+++ 1 course 2 semester/practice2/practice2.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <time.h>
+
+using namespace std;
+
+void quickCopy(int *mas_a, int *mas_b, int N) {
+    unsigned long long* source = (unsigned long long*)mas_a;
+    unsigned long long* place = (unsigned long long*)mas_b;
+
+    // копируем по 8 байт
+    for (int i = 0; i < N / sizeof(long long); i++) {
+        *(place++) = *(source++);
+    }
+
+    unsigned char* source2 = (unsigned char*)source;
+    unsigned char* place2 = (unsigned char*)place;
+
+    // копируем остаток по 1 байту
+    for (int i = 0; i < N % sizeof(long long); i++) {
+        *(place2++) = *(source2++);
+    }
+}
+
+void showElements(int* mas, int N) {
+    for (int i = 0; i < N; i++) {
+        cout << mas[i] << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    const int N = 100;
+    int mas_a[N] = { 1, 2, 3, 4, 5};
+    int mas_b[N] = { 0 };
+
+    quickCopy(mas_a, mas_b, sizeof(mas_a));
+
+    showElements(mas_b, N);
+}
